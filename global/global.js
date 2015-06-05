c36d9a4f306c82c33e8dd1dec519da97ac239260
Jay <punchjay@gmail.com>
2015-06-05 14:58:46 -0700

    updates content

--- global/global.js
+++ global/global.js
@@ -27,6 +27,19 @@ $(document).ready(function () {
     }());
     //end polyfill
 
+    //twitter feed
+    ! function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + "://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+
+    //navigation and scrolling
     var $navBt = $('.nav-bt');
 
     $navBt.on('click', function () {
